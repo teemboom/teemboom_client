f8a77f0ab18ded1b5152f062d5ecd8fcda222815
james-koduah <jameskoduah171@gmail.com>
2024-11-05 07:52:48 +0000

    Testing bug

--- teemboom_comments.js
+++ teemboom_comments.js
@@ -156,6 +156,7 @@ class teemboomCommentsClass{
 		let user = this.get_user()
 		if (!user) return
 		comment_box.value = ''
+		console.log(user)
 		fetch(`${this.teemboom_url}/submit_comment`, {
 			'headers': {'Content-type': 'application/json'},
 			'method': 'POST',
@@ -290,7 +291,7 @@ class teemboomCommentsClass{
 			return this.user
 		}
 		if (this.config.identification == '2'){
-			if (!this.user || !this.user.username){ // If the user is not signed in
+			if (this.user === false || !this.user.username || this.user.username === null){ // If the user is not signed in
 				let markup = [
 					['header', 'Enter Your Username', false],
 					['input', false, {
@@ -305,7 +306,7 @@ class teemboomCommentsClass{
 			return this.user
 		}
 		if (this.config.identification == '3'){
-			if (!this.user || !this.user.username){ // If the user is not signed in
+			if (this.user === false || !this.user.username || this.user.username === null){ // If the user is not signed in
 				let markup = [
 					['label', 'Username', false],
 					['input', false, {'placeholder': 'Username', 'id': 'teemboom_lev3_username'}],
@@ -320,7 +321,7 @@ class teemboomCommentsClass{
 			return this.user
 		}
 		if (this.config.identification == '4'){
-			if (!this.user || !this.user.username){
+			if (this.user === false || !this.user.username || this.user.username === null){
 				let markup = [
 					['header', 'Login', false],
 					['button', '<svg width="64px" height="64px" viewBox="0 0 32 32" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M23.75,16A7.7446,7.7446,0,0,1,8.7177,18.6259L4.2849,22.1721A13.244,13.244,0,0,0,29.25,16" fill="#00ac47"></path><path d="M23.75,16a7.7387,7.7387,0,0,1-3.2516,6.2987l4.3824,3.5059A13.2042,13.2042,0,0,0,29.25,16" fill="#4285f4"></path><path d="M8.25,16a7.698,7.698,0,0,1,.4677-2.6259L4.2849,9.8279a13.177,13.177,0,0,0,0,12.3442l4.4328-3.5462A7.698,7.698,0,0,1,8.25,16Z" fill="#ffba00"></path><polygon fill="#2ab2db" points="8.718 13.374 8.718 13.374 8.718 13.374 8.718 13.374"></polygon><path d="M16,8.25a7.699,7.699,0,0,1,4.558,1.4958l4.06-3.7893A13.2152,13.2152,0,0,0,4.2849,9.8279l4.4328,3.5462A7.756,7.756,0,0,1,16,8.25Z" fill="#ea4435"></path><polygon fill="#2ab2db" points="8.718 18.626 8.718 18.626 8.718 18.626 8.718 18.626"></polygon><path d="M29.25,15v1L27,19.5H16.5V14H28.25A1,1,0,0,1,29.25,15Z" fill="#4285f4"></path></g></svg><p>Google</p>', {
